f0df91c19767ed0e9fc8472abc581e22379a2e88
Ethan Blackwood <ethanbblackwood@gmail.com>
2022-09-28 16:18:40 -0400

    Update settings to re-enable redirect

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -40,12 +40,10 @@ if (defined('MED_SERVER') && constant('MED_SERVER') && php_sapi_name() !== 'cli'
   $primary_domain = 'www.neuronetupenn.org';
   $requires_redirect = FALSE;
 
-/*
   if ($_SERVER['HTTP_HOST'] !== $primary_domain) {
     $requires_redirect = TRUE;
     $redirect_path = $_SERVER['REQUEST_URI'];
   }
-*/
 
   if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off'
       && $_SERVER['SERVER_PORT'] === 80) {
@@ -61,7 +59,6 @@ if (defined('MED_SERVER') && constant('MED_SERVER') && php_sapi_name() !== 'cli'
 
   // Drupal 8 Trusted Host Settings
   if (is_array($settings)) {
-    $settings['trusted_host_patterns'] = array('^' . preg_quote($primary_domain) . '$',
-'^' . preg_quote('hosting2.pmacs.upenn.edu') . '$');
+    $settings['trusted_host_patterns'] = array('^' . preg_quote($primary_domain) . '$');
   }
 }
